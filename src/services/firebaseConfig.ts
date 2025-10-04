4cb9140f7139826cbb2e09a0a6a91bd0af1baade
NigelGM <nigelgmorais@gmail.com>
2025-10-04 19:20:12 +0800

    Update Firebase config file

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,24 +1,24 @@
 // frontend/src/services/firebaseConfig.ts
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBP8zixixwSYeYqkO_6TcJ7MNn0VftqQIM",
   authDomain: "project-pneurelief.firebaseapp.com",
   projectId: "project-pneurelief",
   storageBucket: "project-pneurelief.firebasestorage.app",
   messagingSenderId: "149894939350",
-  appId: "1:149894939350:web:0f70f0722b65fc1ee0ff77",
-  measurementId: "G-13H29TEZM1"
+  appId: "1:149894939350:web:6f70f0722b65fc1ee0ff77",
+  measurementId: "G-13H29TEZM1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export { app, analytics };
+// Analytics is optional (only works on web)
+isSupported().then((yes) => {
+  if (yes) getAnalytics(app);
+});
+
+export default app;
+
