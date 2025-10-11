4393c6738690d539afe2eecf2861703bf5fb9da0
choijiwonsoc <choijw@comp.nus.edu.sg>
2025-10-11 22:09:49 +0800

    firebase auth cnfigs

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,15 +3,24 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics, isSupported } from 'firebase/analytics';
+import {
+  AUTH_FIREBASE_API_KEY,
+  AUTH_FIREBASE_AUTH_DOMAIN,
+  AUTH_FIREBASE_PROJECT_ID,
+  AUTH_FIREBASE_STORAGE_BUCKET,
+  AUTH_FIREBASE_MESSAGING_SENDER_ID,
+  AUTH_FIREBASE_APP_ID,
+  AUTH_FIREBASE_MEASUREMENT_ID,}
+ from "../config"
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: AUTH_FIREBASE_API_KEY,
+  authDomain: AUTH_FIREBASE_AUTH_DOMAIN,
+  projectId: AUTH_FIREBASE_PROJECT_ID,
+  storageBucket:AUTH_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: AUTH_FIREBASE_MESSAGING_SENDER_ID,
+  appId: AUTH_FIREBASE_APP_ID,
+  measurementId: AUTH_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
